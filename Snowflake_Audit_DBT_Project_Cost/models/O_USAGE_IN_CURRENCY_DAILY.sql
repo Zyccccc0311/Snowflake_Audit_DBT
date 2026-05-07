3f4b9bc891c63598f6657e5240d63f2f5faca5e9
Zyccccc0311 <youchen.zhang@capgemini.com>
2026-05-07 06:59:51 +0000

    20260507

--- Snowflake_Audit_DBT_Project_Cost/models/O_USAGE_IN_CURRENCY_DAILY.sql
+++ Snowflake_Audit_DBT_Project_Cost/models/O_USAGE_IN_CURRENCY_DAILY.sql
@@ -1,63 +1,34 @@
 {{ config(materialized='table') }}
-
-with base as (
-
-    select
-        ORGANIZATION_NAME,
-        CONTRACT_NUMBER,
-        ACCOUNT_NAME,
-        ACCOUNT_LOCATOR,
-        "REGION",
-        SERVICE_LEVEL,
-        USAGE_DATE,
-        USAGE_TYPE,
-        "USAGE",
-        CURRENCY,
-        USAGE_IN_CURRENCY,
-        BALANCE_SOURCE,
-        BILLING_TYPE,
-        RATING_TYPE,
-        SERVICE_TYPE,
-        IS_ADJUSTMENT
-    from {{ source('organization_usage', 'USAGE_IN_CURRENCY_DAILY') }}
-
+WITH usage_in_currency AS(
+SELECT
+    ORGANIZATION_NAME, 
+    CONTRACT_NUMBER, 
+    ACCOUNT_NAME, 
+    ACCOUNT_LOCATOR, 
+    REGION, 
+    SERVICE_LEVEL, 
+    USAGE_DATE, 
+    USAGE_TYPE, 
+    CAST(USAGE AS DECIMAL(15,6)) as "USAGE", 
+    CURRENCY, 
+    CAST(USAGE_IN_CURRENCY AS DECIMAL(15,6)) as USAGE_IN_CURRENCY, 
+    BALANCE_SOURCE, 
+    BILLING_TYPE, 
+    RATING_TYPE, 
+    SERVICE_TYPE, 
+    IS_ADJUSTMENT
+FROM {{ source('organization_usage', 'USAGE_IN_CURRENCY_DAILY') }}
 ),
-
-cloud_final as (
-
-    select
-        any_value(ORGANIZATION_NAME) as ORGANIZATION_NAME,
-        any_value(CONTRACT_NUMBER) as CONTRACT_NUMBER,
-        ACCOUNT_NAME,
-        ACCOUNT_LOCATOR,
-        any_value("REGION") as "REGION",
-        any_value(SERVICE_LEVEL) as SERVICE_LEVEL,
-        USAGE_DATE,
-        'cloud final' as USAGE_TYPE,
-        sum("USAGE") as "USAGE",
-        any_value(CURRENCY) as CURRENCY,
-        sum(USAGE_IN_CURRENCY) as USAGE_IN_CURRENCY,
-        any_value(BALANCE_SOURCE) as BALANCE_SOURCE,
-        any_value(BILLING_TYPE) as BILLING_TYPE,
-        any_value(RATING_TYPE) as RATING_TYPE,
-        'CLOUD_SERVICES' as SERVICE_TYPE,
-        null as IS_ADJUSTMENT
-    from base
-    where upper(SERVICE_TYPE) = 'CLOUD_SERVICES'
-    and lower(USAGE_TYPE) in ('cloud services','adj for incl cloud services')
-    group by
-        ACCOUNT_LOCATOR,
-        ACCOUNT_NAME,
-        USAGE_DATE
-
+rate_cny_sek AS (
+SELECT * 
+FROM {{ source('cost', 'D_RATE') }}
 )
 
-select
-    *
-from base
+select 
+    usage_in_currency.*, 
+    CAST(rate_cny_sek.RATE*usage_in_currency.USAGE_IN_CURRENCY AS DECIMAL(15,6)) AS USAGE_IN_CURRENCY_SEK
+FROM usage_in_currency
+LEFT JOIN rate_cny_sek
+ON
+rate_cny_sek.RATE_DATE=usage_in_currency.USAGE_DATE2026595
 
-union all
-
-select
-    *
-from cloud_final
--- Snowflake_Audit_DBT_Project_Cost/models/O_WAREHOUSE_METERING_HISTORY.sql
+++ Snowflake_Audit_DBT_Project_Cost/models/O_WAREHOUSE_METERING_HISTORY.sql
@@ -1,5 +1,42 @@
 {{ config(materialized='table') }}
-
+with 
+warehouse_metering as(
 select
-    * 
+    ORGANIZATION_NAME, 
+    ACCOUNT_NAME, 
+    REGION, 
+    SERVICE_TYPE, 
+    START_TIME, 
+    END_TIME,
+    TO_DATE(CONVERT_TIMEZONE('UTC', START_TIME)) as START_DATE,
+    TO_DATE(CONVERT_TIMEZONE('UTC', END_TIME)) as END_DATE,
+    case
+        when START_DATE = END_DATE then true
+        else false
+    end as IS_SAME_DAY,
+    TO_DATE(GREATEST(START_DATE,END_DATE)) as SERVICE_DATE, 
+    WAREHOUSE_ID, 
+    WAREHOUSE_NAME, 
+    cast(CREDITS_USED as decimal(15,6)) as CREDITS_USED, 
+    cast(CREDITS_USED_COMPUTE as decimal(15,6)) as CREDITS_USED_COMPUTE, 
+    cast(CREDITS_USED_CLOUD_SERVICES as decimal(15,6)) as CREDITS_USED_CLOUD_SERVICES, 
+    ACCOUNT_LOCATOR
 from {{ source('organization_usage', 'WAREHOUSE_METERING_HISTORY') }}
+),
+credit_rate as(
+select *
+from {{ source('organization_usage', 'RATE_SHEET_DAILY') }}
+where USAGE_TYPE<>'adj for incl cloud services'
+)
+
+select 
+warehouse_metering.*,
+credit_rate.EFFECTIVE_RATE
+from warehouse_metering
+left join credit_rate
+on 
+credit_rate.ACCOUNT_NAME=warehouse_metering.ACCOUNT_NAME
+and
+credit_rate.SERVICE_TYPE=warehouse_metering.SERVICE_TYPE
+and
+credit_rate.DATE=warehouse_metering.SERVICE_DATE
